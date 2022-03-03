e4e067183467750eadb53bde75afde8f082480c0
Kjetil Valle <kjetil.valle@gmail.com>
2022-03-03 20:08:53 +0100

    refactor with view bindings

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -2,59 +2,55 @@ package com.example.helloworldapp
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.helloworldapp.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val helloButton: Button = findViewById(R.id.helloButton)
-        val octoButton: Button = findViewById(R.id.octopusButton)
-        val elephantButton: Button = findViewById(R.id.elephantButton)
-        val kittenButton: Button = findViewById(R.id.kittenButton)
-        val nameField: EditText = findViewById(R.id.whoToGreetTextField);
-        val image: ImageView = findViewById(R.id.kittyImage)
-
-        octoButton.setOnClickListener {
-            image.setImageResource(R.drawable.octopus)
-            image.alpha = 0.6f
-            Log.v(TAG, "Octopus was chosen.")
+        binding.animalsRadioButtonGroup.setOnCheckedChangeListener { _, option ->
+            when (option) {
+                R.id.animals_radio_button_option_kitten -> {
+                    binding.kittyImage.setImageResource(R.drawable.kitty)
+                    binding.kittyImage.alpha = 0.4f
+                    Log.v(TAG, "Kitty was chosen.")
+                }
+                R.id.animals_radio_button_option_octopus -> {
+                    binding.kittyImage.setImageResource(R.drawable.octopus)
+                    binding.kittyImage.alpha = 0.6f
+                    Log.v(TAG, "Octopus was chosen.")
+                }
+                R.id.animals_radio_button_option_elephant -> {
+                    binding.kittyImage.setImageResource(R.drawable.elephant)
+                    binding.kittyImage.alpha = 0.8f
+                    Log.v(TAG, "Elephant was chosen.")
+                }
+            }
         }
 
-        kittenButton.setOnClickListener {
-            image.setImageResource(R.drawable.kitty)
-            image.alpha = 0.4f
-            Log.v(TAG, "Kitty was chosen.")
-        }
+        binding.helloButton.setOnClickListener {
+            binding.helloOutputText.text = "Hello, ${binding.whoToGreetTextField.text}!"
 
-        elephantButton.setOnClickListener {
-            image.setImageResource(R.drawable.elephant)
-            image.alpha = 0.8f
-            Log.v(TAG, "Elephant was chosen.")
-        }
-
-        helloButton.setOnClickListener {
-            val outputTextView: TextView = findViewById(R.id.helloOutputText);
-            outputTextView.text = "Hello, ${nameField.text}!"
-
-            image.pivotX = image.width.toFloat() / 2
-            image.pivotY = image.height.toFloat() / 2
-            image.animate().rotation(15f).setDuration(300).withEndAction {
-                image.animate().rotation(0f).setDuration(300).withEndAction {
-                    image.animate().rotation(15f).setDuration(300).withEndAction {
-                        image.animate().rotation(0f)
+            if (binding.waveSwitch.isChecked) {
+                val image = binding.kittyImage
+                image.pivotX = image.width.toFloat() / 2
+                image.pivotY = image.height.toFloat() / 2
+                image.animate().rotation(15f).setDuration(300).withEndAction {
+                    image.animate().rotation(0f).setDuration(300).withEndAction {
+                        image.animate().rotation(15f).setDuration(300).withEndAction {
+                            image.animate().rotation(0f)
+                        }
                     }
                 }
             }
-
         }
     }
 
